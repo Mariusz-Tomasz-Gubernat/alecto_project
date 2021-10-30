b358463c6b581a1d669bf53a65c7b5cef51acf68
Mariusz Gubernat <mariusz.gubernat@protonmail.com>
2021-10-30 15:53:38 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const Options = [
+const options = [
 
   sectionAbout = document.querySelector('.about-us'),
   navigation = document.querySelector('.second-nav'),
@@ -9,84 +9,83 @@ const Options = [
   allBtn = document.querySelectorAll('p.button'),
   allModals = document.querySelectorAll('.wrapper-PopUp'),
   showMoreAboutWrappers = document.querySelector('.about-us .wrapper-about-two'),
+  activeNavigationClass = "second-nav active showMenu",
 
 ]
 
 const moreLessAboutMeColumn = function () {
 
-  btnMore.addEventListener('click',function(){
+  btnMore.addEventListener('click', function () {
 
     showMoreAboutWrappers.classList.add('active')
     btnMore.classList.add('btn-nonActive');
 
   });
-    
-    btnLess.addEventListener('click',function(){
 
-      showMoreAboutWrappers.classList.remove('active');
-      btnMore.classList.remove('btn-nonActive');
+  btnLess.addEventListener('click', function () {
 
-    });    
+    showMoreAboutWrappers.classList.remove('active');
+    btnMore.classList.remove('btn-nonActive');
+
+  });
 }
 
 const changeMenu = function () {
 
-  window.addEventListener('scroll',function(){
-  
-  const topPos = window.scrollY;
-  const topAbout = sectionAbout.offsetTop; 
-  
-  if ( topAbout <= topPos) {
-    navigation.classList.add('active')
-    bars.classList.add('active');
-  }
-  
-  else{
-  
-    navigation.classList.remove('active')
-    bars.classList.remove('active');
-    bars.classList.remove('pastShowMenu');
-    navigation.classList.remove('showMenu');
-  
-  }
-})
+  window.addEventListener('scroll', function () {
+
+    const topPos = window.scrollY;
+    const topAbout = sectionAbout.offsetTop;
+
+    if (topAbout <= topPos) {
+      navigation.classList.add('active')
+      bars.classList.add('active');
+    } else {
+
+      navigation.classList.remove('active')
+      bars.classList.remove('active');
+      bars.classList.remove('pastShowMenu');
+      navigation.classList.remove('showMenu');
+
+    }
+  })
 }
+const showMenu = function () {
 
-const showMenu = function() {
+  bars.addEventListener('click', function () {
 
-  bars.addEventListener('click',function(){
-  
     bars.classList.toggle('pastShowMenu');
     navigation.classList.toggle('showMenu');
-  
-  });   
+    navigation.classList.toggle('hideMenu');
+
+  });
 }
 
-const showModal = function() {
+const showModal = function () {
 
-  for(let btn_ of allBtn) {
-  document.querySelector('p.button')
+  for (let btn_ of allBtn) {
+    document.querySelector('p.button')
 
-  btn_.addEventListener('click',function(){
-  
-    const btnId = btn_.getAttribute('id').replace('#','')
-      
-      for(let modal of allModals) {
+    btn_.addEventListener('click', function () {
+
+      const btnId = btn_.getAttribute('id').replace('#', '')
+
+      for (let modal of allModals) {
         document.querySelector('wrapper-PopUp');
 
         const modalId = modal.getAttribute('id')
 
-        if(btnId == modalId) {
+        if (btnId == modalId) {
           modal.classList.add('active')
         }
 
-        for(let CloseModalBars of allCloseModalBars){
+        for (let CloseModalBars of allCloseModalBars) {
           document.querySelector('.fa-times')
 
-          CloseModalBars.addEventListener('click',function(){
+          CloseModalBars.addEventListener('click', function () {
             modal.classList.remove('active')
           })
-        } 
+        }
       }
     });
   }
